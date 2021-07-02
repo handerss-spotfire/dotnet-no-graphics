20d2fb937b19b14ae042f8b17175f8082556d16d
Henrik Andersson <handerss@tibco.com>
2021-07-02 15:26:56 +0200

    Minimal example produces no graphics on Linux
    This is a minimal example which shows different behavior when run on
    Linux vs Windows. Running on Windows produces 2 images with blue
    rectangles (out1.png and out2.png) contrary to Linux where out1.png is
    empty. out2.png is a blue rectangle on Linux if the bitmap get saved to
    disk before it is cloned (the copy saved to disk is removed before the
    clone operation).

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace NoGraphics
+{
+    class Program
+    {
+        const string InPath = "image.png";
+        const string NoGraphicsPath = "out1.png";
+        const string GraphicsPath = "out2.png";
+        const string IntermidiatePath = "intermidiate.png";
+
+        static void Main(string[] args)
+        {
+            var image = Image.FromFile(InPath);
+            var bitmap = UseGraphics(image);
+
+            var clone = bitmap.Clone() as Image;
+            clone.Save(NoGraphicsPath);
+
+            bitmap.Save(IntermidiatePath);
+            File.Delete(IntermidiatePath);
+            var clone2 = bitmap.Clone() as Image;
+            clone2.Save(GraphicsPath);
+        }
+
+        private static Bitmap UseGraphics(Image image)
+        {
+            var bitmap = new Bitmap(image.Width, image.Height);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.DrawImage(image, 0, 0);
+            }
+            return bitmap;
+        }
+    }
+}
